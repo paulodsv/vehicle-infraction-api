139f88a0cd79f5b537a3fd3284bf99e1885de4ba
paulodsv <paulovieiradias064@gmail.com>
2026-04-04 10:09:58 -0300

    chore: add core configuration module

---
+++ app/core/config.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+settings = Settings()
