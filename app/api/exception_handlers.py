7a550674b6dcbdee89668ccf7615f4b448c10b7a
paulodsv <paulovieiradias064@gmail.com>
2026-04-07 13:42:34 -0300

    feat: add new exception handlers

--- app/api/exception_handlers.py
+++ app/api/exception_handlers.py
@@ -1,9 +1,17 @@
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from app.domain.exceptions import AlreadyRegisteredUser
+from app.domain.exceptions import AlreadyRegisteredUser, InvalidUserEmail, InvalidUserPassword
 
 def register_exception_handlers(app):
 
     @app.exception_handler(AlreadyRegisteredUser)
     async def handler_already_registered_user(request: Request, exc: AlreadyRegisteredUser):
         return JSONResponse(status_code=409, content={"detail": str(exc)})
+    
+    @app.exception_handler(InvalidUserPassword)
+    async def handler_invalid_user_password(request: Request, exc: InvalidUserPassword):
+        return JSONResponse(status_code=400, content={"detail": str(exc)})
+    
+    @app.exception_handler(InvalidUserEmail)
+    async def handler_invalid_user_password(request: Request, exc: InvalidUserEmail):
+        return JSONResponse(status_code=404, content={"detail": str(exc)})
