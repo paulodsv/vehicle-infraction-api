fec8a4f5b0e7b60505ea868746b295c7fbf6d243
paulodsv <paulovieiradias064@gmail.com>
2026-04-07 10:20:07 -0300

    feat: add new auth config with hash pass

---
+++ app/core/security.py
@@ -0,0 +1,24 @@
+from passlib.context import CryptContext
+from jose import jwt 
+from datetime import datetime, timedelta
+from app.core.config import settings
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    hashed_password = pwd_context.hash(password)
+    return hashed_password
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    valid_password = pwd_context.verify(plain_password, hashed_password)
+    return valid_password
+
+
+def create_access_token(data: dict) -> str:
+    return jwt.encode(
+        {"sub": data["email"], "exp": datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)},
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
