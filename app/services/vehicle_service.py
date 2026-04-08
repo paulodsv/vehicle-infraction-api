5d3e55ce35a458ffb60ed73ee4875c57d89a952a
paulodsv <paulovieiradias064@gmail.com>
2026-04-08 10:13:00 -0300

    feat: add new vehicle service class with basic methods

---
+++ app/services/vehicle_service.py
@@ -0,0 +1,46 @@
+from app.repositories.vehicle_repository import VehicleRepository
+from app.models.vehicle import Vehicle
+from app.schemas.vehicle import VehicleCreate, TipoVeiculo, VehicleUpdate
+from app.domain.exceptions import AlreadyRegisteredVehicle, NotRegisteredVehicle
+
+class VehicleService():
+    def __init__(self, vehicle_repo: VehicleRepository):
+        self.vehicle_repo = vehicle_repo
+    
+    def save_vehicle_service(self, vehicle_data: VehicleCreate) -> Vehicle:
+        vehicle = self.vehicle_repo.get_vehicle_by_plate(vehicle_data.plate)
+        if vehicle:
+            raise AlreadyRegisteredVehicle("Já existe um veículo cadastrado com a placa informada")
+        
+        return self.vehicle_repo.save_vehicle(vehicle_data)
+    
+    def get_vehicle_by_plate_service(self, plate: str) -> Vehicle:
+        vehicle = self.vehicle_repo.get_vehicle_by_plate(plate)
+        if not vehicle:
+            raise NotRegisteredVehicle("Veículo com a placa informada não cadastrado no sistema")
+        
+        return vehicle
+            
+    def get_vehicles_by_type_service(self, type: TipoVeiculo) -> list[Vehicle]:
+        vehicle = self.vehicle_repo.get_vehicles_by_type(type)
+        return vehicle
+    
+    def get_vehicle_by_id_service(self, id: int) -> Vehicle:
+        vehicle = self.vehicle_repo.get_vehicle_by_id(id)
+        if not vehicle:
+            raise NotRegisteredVehicle("Veículo com o id informado não cadastrado no sistema")
+        
+        return vehicle
+    
+    def update_vehicle_service(self, id: int, vehicle_data: VehicleUpdate) -> Vehicle:
+        vehicle = self.vehicle_repo.get_vehicle_by_id(id)
+        if not vehicle:
+            raise NotRegisteredVehicle("Veículo com o id informado não cadastrado no sistema")
+        
+        return self.vehicle_repo.update_vehicle(id, vehicle_data)
+    
+    def get_all_vehicles_service(self) -> list[Vehicle]:
+        return self.vehicle_repo.get_all_vehicles()
+    
+    def get_all_active_vehicles_service(self) -> list[Vehicle]:
+        return self.vehicle_repo.get_all_active_vehicles()
