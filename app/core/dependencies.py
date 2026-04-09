3607fcb8bcf8cb36f5510ac094608b2bac40a5c5
paulodsv <paulovieiradias064@gmail.com>
2026-04-09 14:07:41 -0300

    feat: add new get current user dep

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,4 +1,10 @@
 from app.core.database import SessionLocal
+from jose import jwt, JWTError
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.core.config import settings
 
 def get_db():
     db = SessionLocal()
@@ -6,3 +12,20 @@ def get_db():
         yield db
     finally:
         db.close()
+
+oauth2_scheme = OAuth2AuthorizationCodeBearer(tokenUrl="/auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email = payload.get("sub")
+        if email is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    
+    return user
