a1fc538d4325fc22d3c7ca165544f83c3a3b2ba3
paulodsv <paulovieiradias064@gmail.com>
2026-04-17 11:01:35 -0300

    feat: add endpoint auth to all vehicle methods

--- app/routers/vehicle.py
+++ app/routers/vehicle.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
 from app.schemas.vehicle import VehicleCreate, TipoVeiculo, VehicleUpdate, VehicleResponse
-from app.core.dependencies import get_vehicle_service
+from app.core.dependencies import get_vehicle_service, get_current_user
 from app.services.vehicle_service import VehicleService
 from typing import List
+from app.models.user import User
 
 vehicle_router = APIRouter(prefix="/vehicles", tags=["Vehicles"])
 
@@ -14,7 +15,9 @@ vehicle_router = APIRouter(prefix="/vehicles", tags=["Vehicles"])
                          201: {"description": "Veículo registrado com sucesso"},
                          409: {"description": "Veículo com essa placa já registrado no sistema"}
                      })
-def save_vehicle(vehicle_data: VehicleCreate, service: VehicleService = Depends(get_vehicle_service)):
+def save_vehicle(vehicle_data: VehicleCreate, 
+                 service: VehicleService = Depends(get_vehicle_service), 
+                 current_user: User = Depends(get_current_user)):
     return service.save_vehicle_service(vehicle_data)
 
 @vehicle_router.get("/{plate}", response_model=VehicleResponse,
@@ -25,20 +28,10 @@ def save_vehicle(vehicle_data: VehicleCreate, service: VehicleService = Depends(
                         200: {"description": "Veículo retornado com sucesso"},
                         404: {"description": "Placa informada não encontrada no sistema"}
                     })
-def get_vehicle_by_plate(plate: str, service: VehicleService = Depends(get_vehicle_service)):
+def get_vehicle_by_plate(plate: str, 
+                         service: VehicleService = Depends(get_vehicle_service), 
+                         current_user: User = Depends(get_current_user)):
     return service.get_vehicle_by_plate_service(plate)
-
-
-@vehicle_router.get("/{id}", response_model=VehicleResponse,
-                    summary="Busca um veículo por id",
-                    description="Retorna um veículo cadastrado no sistema pelo id",
-                    operation_id="getVehicleById",
-                    responses={
-                        200: {"description": "Veículo retornado com sucesso"},
-                        404: {"description": "ID informado não encontrado no sistema"}
-                    })
-def get_vehicle_by_id(id: int, service: VehicleService = Depends(get_vehicle_service)):
-    return service.get_vehicle_by_id_service(id)
 
 @vehicle_router.patch("/", response_model=VehicleResponse,
                      summary="Atualiza um veículo",
@@ -48,7 +41,9 @@ def get_vehicle_by_id(id: int, service: VehicleService = Depends(get_vehicle_ser
                          201: {"description": "Veículo atualizado com sucesso"},
                          404: {"description": "Veículo com o ID informado não existe"}
                      })
-def update_vehicle(id: int, updated_vehicle_data: VehicleUpdate, service: VehicleService = Depends(get_vehicle_service)):
+def update_vehicle(id: int, updated_vehicle_data: VehicleUpdate, 
+                   service: VehicleService = Depends(get_vehicle_service), 
+                   current_user: User = Depends(get_current_user)):
     return service.update_vehicle_service(id, updated_vehicle_data)
 
 @vehicle_router.get("/", response_model=List[VehicleResponse],
@@ -60,5 +55,6 @@ def update_vehicle(id: int, updated_vehicle_data: VehicleUpdate, service: Vehicl
                     })
 def get_all_vehicles(type: TipoVeiculo | None = None, 
                            is_active: bool | None = None, 
-                           service: VehicleService = Depends(get_vehicle_service)):
+                           service: VehicleService = Depends(get_vehicle_service),
+                           current_user: User = Depends(get_current_user)):
     return service.get_all_vehicles_service(type, is_active)
