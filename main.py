d6602c3202b55c91def8773e534b47b5f03d46a1
paulodsv <paulovieiradias064@gmail.com>
2026-04-17 10:09:25 -0300

    feat: add FastAPI application entry point (main)

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from app.routers.auth import auth_router
+from app.api.exception_handlers import register_exception_handlers
+
+
+app = FastAPI(title="Vehicle Infractions Service", description="""
+              API para gerenciamento e monitoramento de infrações de trânsito em frotas de transporte.
+
+              Permite o cadastro e gerenciamento de veículos, autenticação de usuários e consulta
+              de infrações individuais ou consolidadas por frota.
+
+              Projetada para atender transportadoras que necessitam de maior controle sobre multas,
+              facilitando a análise, organização e acompanhamento das infrações de seus veículos.
+
+              Conta com integração a sistemas externos para obtenção de dados atualizados de infrações.
+              """,
+              contact={
+                  "name": "Paulo Henrique",
+                  "email": "paulovieiradias064@gmail.com"
+              },
+              docs_url="/docs",
+              redoc_url="/redoc",
+              version="1.0.0")
+
+register_exception_handlers(app)
+app.include_router(auth_router)
+
+
