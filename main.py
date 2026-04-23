f20b4fba293ca243b58c28920b238e13432f2b89
paulodsv <paulovieiradias064@gmail.com>
2026-04-23 15:24:02 -0300

    refactor: add swagger css customization

--- main.py
+++ main.py
@@ -5,26 +5,32 @@ from app.routers.infraction import infractions_router
 from app.routers.senatran import senatran_router
 from app.routers.user import user_router
 from app.api.exception_handlers import register_exception_handlers
+from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.docs import get_swagger_ui_html
 
 
 app = FastAPI(title="Vehicle Infractions Service", description="""
-              API para gerenciamento e monitoramento de infrações de trânsito em frotas de transporte.
-
-              Permite o cadastro e gerenciamento de veículos, autenticação de usuários e consulta
-              de infrações individuais ou consolidadas por frota.
-
-              Projetada para atender transportadoras que necessitam de maior controle sobre multas,
-              facilitando a análise, organização e acompanhamento das infrações de seus veículos.
-
-              Conta com integração a sistemas externos para obtenção de dados atualizados de infrações.
-              """,
+API para gerenciamento e monitoramento de infrações de trânsito em frotas de transporte.
+Permite o cadastro e gerenciamento de veículos, autenticação de usuários e consulta de infrações individuais ou consolidadas por frota.
+Projetada para atender transportadoras que necessitam de maior controle sobre multas, facilitando a análise, organização e acompanhamento das infrações de seus veículos.
+Conta com integração a sistemas externos para obtenção de dados atualizados de infrações.
+""",
               contact={
                   "name": "Paulo Henrique",
                   "email": "paulovieiradias064@gmail.com"
               },
-              docs_url="/docs",
-              redoc_url="/redoc",
+              docs_url=None,
               version="1.0.0")
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger():
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",
+        title="Vehicle Infractions Service",
+        swagger_css_url="/static/swagger-custom.css"
+    )
 
 register_exception_handlers(app)
 app.include_router(auth_router)
