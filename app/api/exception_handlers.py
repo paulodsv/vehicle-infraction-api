6040bfb512db3cc431b884c7deb4c26e175c11ae
paulodsv <paulovieiradias064@gmail.com>
2026-04-23 10:44:45 -0300

    refactor: change status code on exceptions

--- app/api/exception_handlers.py
+++ app/api/exception_handlers.py
@@ -10,15 +10,15 @@ def register_exception_handlers(app):
     
     @app.exception_handler(InvalidUserPassword)
     async def handler_invalid_user_password(request: Request, exc: InvalidUserPassword):
-        return JSONResponse(status_code=400, content={"detail": str(exc)})
+        return JSONResponse(status_code=401, content={"detail": str(exc)})
     
     @app.exception_handler(InvalidUserEmail)
     async def handler_invalid_user_email(request: Request, exc: InvalidUserEmail):
-        return JSONResponse(status_code=404, content={"detail": str(exc)})
+        return JSONResponse(status_code=401, content={"detail": str(exc)})
     
     @app.exception_handler(NotRegisteredVehicle)
     async def handler_not_registered_vehicle(request: Request, exc: NotRegisteredVehicle):
-        return JSONResponse(status_code=404, content={"detail": str(exc)})
+        return JSONResponse(status_code=400, content={"detail": str(exc)})
     
     @app.exception_handler(AlreadyRegisteredVehicle)
     async def handler_already_registered_vehicle(request: Request, exc: AlreadyRegisteredVehicle):
