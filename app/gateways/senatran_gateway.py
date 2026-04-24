f03f96e6dec6d8bb0d36b83d11166db73564961e
paulodsv <paulovieiradias064@gmail.com>
2026-04-24 10:30:03 -0300

    refactor: gateway methods now returns DTOs instead raw json

--- app/gateways/senatran_gateway.py
+++ app/gateways/senatran_gateway.py
@@ -1,6 +1,9 @@
 from app.core.config import settings
 import httpx
 from app.schemas.senatran import SenatranInfractionDetailsQuery, SenatranInfractionQuery
+from app.dtos.senatran.infraction_dto import SenatranInfractionDTO, SenatranResponseDTO
+from app.dtos.senatran.details_dto import SenatranDetailsDTO
+from app.gateways.exceptions import ExternalServiceError
 
 class SenatranGateway():
     def __init__(self):
@@ -11,14 +14,27 @@ class SenatranGateway():
         self.base_infractions_details_url = settings.INFOSIMPLES_DETAILS_URL
 
     def get_infraction_by_plate(self, query: SenatranInfractionQuery):
-        infractions = httpx.post(self.base_infractions_url, data={"token": self.token, 
+        response = httpx.post(self.base_infractions_url, data={"token": self.token, 
                                                       "placa": query.plate, 
                                                       "cnpj": query.cnpj, 
                                                       "login_cpf": self.gov_cpf, 
                                                       "login_senha": self.gov_senha}, 
                                                       timeout=45.0)
-        response = infractions.json()
-        return response
+        data = response.json()
+
+        if data.get("errors"):
+            raise ExternalServiceError(data["errors"][0])
+        
+        raw = data["data"][0]
+
+        infractions = [
+            SenatranInfractionDTO(item)
+            for item in raw["infracoes"]
+        ]
+
+        total = raw["total_infracoes"]
+
+        return SenatranResponseDTO(infractions, total)
     
     def get_infraction_details(self, query: SenatranInfractionDetailsQuery):
         details = httpx.post(self.base_infractions_details_url, data={"token": self.token, 
@@ -29,4 +45,7 @@ class SenatranGateway():
                                                                       "login_senha": self.gov_senha},
                                                                       timeout=45.0)
         response = details.json()
-        return response
+        raw = response["data"][0]
+        details_dto = SenatranDetailsDTO(raw)
+        
+        return details_dto
