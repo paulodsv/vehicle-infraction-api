1a1f4e10ded2c9da618fdf11eb3be01dc94e68c7
paulodsv <paulovieiradias064@gmail.com>
2026-04-24 10:27:16 -0300

    refactor: remove dto mapping on service

--- app/services/senatran_service.py
+++ app/services/senatran_service.py
@@ -3,7 +3,7 @@ from app.schemas.infraction import InfractionQueryCreate, InfractionCreate, Infr
 from app.schemas.senatran import SenatranInfractionQuery, SenatranInfractionDetailsQuery
 from app.repositories.infraction_repository import InfractionRepository
 from app.repositories.vehicle_repository import VehicleRepository
-from app.dtos.senatran.infraction_dto import SenatranInfractionDTO, SenatranInfractionQueryDTO
+from app.dtos.senatran.infraction_dto import SenatranResponseDTO
 from app.dtos.senatran.details_dto import SenatranDetailsDTO
 from app.models.infractions import Infractions
 from app.domain.exceptions import NotRegisteredVehicle
@@ -14,37 +14,32 @@ class SenatranService():
         self.infraction_repo = infraction_repo
         self.vehicles_repo = vehicles_repo
 
-    def consult_infractions_service(self, query: SenatranInfractionQuery, current_user_id: int) -> Infractions:
+    def consult_infractions_service(self, query: SenatranInfractionQuery, current_user_id: int) -> list[Infractions]:
 
         if not query.cnpj:
             plate_cnpj = self.vehicles_repo.get_cnpj_by_plate(query.plate)
             if not plate_cnpj: 
-                raise NotRegisteredVehicle("Veículo não cadastrado no sistema. Favor informar o cnpj correspondente")
+                raise NotRegisteredVehicle("Veículo não cadastrado no sistema. Favor cadastrar antes de realizar qualquer requisição")
             query.cnpj = plate_cnpj
 
-        infractions = self.senatran_gateway.get_infraction_by_plate(query)
-        if not infractions["data"]:
-            raise Exception(infractions["errors"][0])
+        response: SenatranResponseDTO = self.senatran_gateway.get_infraction_by_plate(query)
         
         #Save da query do respectivo usuário
-        query_dto = SenatranInfractionQueryDTO(infractions)
-        infraction_query = InfractionQueryCreate(total_infractions = query_dto.total_infractions, 
+        infraction_query = InfractionQueryCreate(total_infractions = response.total, 
                                                  plate=query.plate, 
                                                  user_id=current_user_id)
         saved_query = self.infraction_repo.save_query(infraction_query)
 
         #Save das infrações
-        for infraction in infractions["data"][0]["infracoes"]:
-            dto = SenatranInfractionDTO(infraction)
-
+        for infraction in response.infractions:
             infraction_create = InfractionCreate(plate = query.plate, 
-                                                infraction_notice_number = dto.notice_number, 
-                                                infraction_code = dto.infraction_code,
-                                                description = dto.description,
-                                                total_amount = dto.total_amount,
-                                                due_date = dto.due_date,
-                                                infraction_key = dto.infraction_key,
-                                                status = dto.status,
+                                                infraction_notice_number = infraction.notice_number, 
+                                                infraction_code = infraction.infraction_code,
+                                                description = infraction.description,
+                                                total_amount = infraction.total_amount,
+                                                due_date = infraction.due_date,
+                                                infraction_key = infraction.infraction_key,
+                                                status = infraction.status,
                                                 query_id = saved_query.id)
             
             self.infraction_repo.save_infraction(infraction_create)
@@ -70,27 +65,26 @@ class SenatranService():
         for infraction in infractions_with_no_details:
             cnpj_infraction = self.vehicles_repo.get_cnpj_by_plate(infraction.plate)
             query_details = SenatranInfractionDetailsQuery(plate=infraction.plate, cnpj=cnpj_infraction, infraction_key=infraction.infraction_key)
-            details = self.senatran_gateway.get_infraction_details(query_details)
-            dto = SenatranDetailsDTO(details)
+            details: SenatranDetailsDTO = self.senatran_gateway.get_infraction_details(query_details)
 
-            infraction_detailed = InfractionDetailedCreate(issuing_authority=dto.issuing_authority,
-                                                           competent_authority=dto.competent_authority,
-                                                           ait_number=dto.ait_number,
-                                                           notification_date=dto.notification_date,
-                                                           defense_deadline=dto.defense_deadline,
-                                                           offender_indication_deadline=dto.offender_indication_deadline,
-                                                           driver_name=dto.driver_name,
-                                                           driver_cnh=dto.driver_cnh,
-                                                           driver_document=dto.driver_document,
-                                                           fine_amount=dto.fine_amount,
-                                                           measurement_taken=dto.measurement_taken,
-                                                           considered_value=dto.considered_value,
-                                                           regulated_limit=dto.regulated_limit,
-                                                           infraction_location=dto.infraction_location,
-                                                           infraction_date=dto.infraction_date,
-                                                           infraction_time=dto.infraction_time,
-                                                           city=dto.city,
-                                                           state=dto.state)
+            infraction_detailed = InfractionDetailedCreate(issuing_authority=details.issuing_authority,
+                                                           competent_authority=details.competent_authority,
+                                                           ait_number=details.ait_number,
+                                                           notification_date=details.notification_date,
+                                                           defense_deadline=details.defense_deadline,
+                                                           offender_indication_deadline=details.offender_indication_deadline,
+                                                           driver_name=details.driver_name,
+                                                           driver_cnh=details.driver_cnh,
+                                                           driver_document=details.driver_document,
+                                                           fine_amount=details.fine_amount,
+                                                           measurement_taken=details.measurement_taken,
+                                                           considered_value=details.considered_value,
+                                                           regulated_limit=details.regulated_limit,
+                                                           infraction_location=details.infraction_location,
+                                                           infraction_date=details.infraction_date,
+                                                           infraction_time=details.infraction_time,
+                                                           city=details.city,
+                                                           state=details.state)
             self.infraction_repo.update_infraction_details(infraction_detailed, infraction.infraction_notice_number)
         return len(infractions_with_no_details)
 
