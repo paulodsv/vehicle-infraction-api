9abdcd1e5a04c1c6f304ac668c43116a4fc4ff24
paulodsv <paulovieiradias064@gmail.com>
2026-04-27 10:50:08 -0300

    feat: create logging setup config

---
+++ app/core/logging_config.py
@@ -0,0 +1,11 @@
+import logging
+import sys
+from app.core.config import settings
+
+def setup_logging() -> None:
+    logging.basicConfig(
+        level=settings.log_level.upper(),
+        format="%(asctime)s | %(levelname)-8s | %(name)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
