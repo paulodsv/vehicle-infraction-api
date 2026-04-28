e7158cf75b88ff7f074f1f5d69f33b40b4b40de9
paulodsv <paulovieiradias064@gmail.com>
2026-04-28 09:16:55 -0300

    refactor: add validation and raise exception on get_infractions_by_plate method

--- app/services/infraction_service.py
+++ app/services/infraction_service.py
@@ -1,15 +1,20 @@
 from app.repositories.infraction_repository import InfractionRepository
 from app.models.infractions import Infractions
 from app.models.infraction_queries import InfractionQueries
-from app.domain.exceptions import UserIdNotFound, QueryIdNotFound
+from app.domain.exceptions import UserIdNotFound, QueryIdNotFound, NotRegisteredVehicle
 from app.repositories.user_repository import UserRepository 
+from app.repositories.vehicle_repository import VehicleRepository
 
 class InfractionService():
-    def __init__(self, infraction_repo: InfractionRepository, user_repo: UserRepository):
+    def __init__(self, infraction_repo: InfractionRepository, user_repo: UserRepository, vehicle_repo = VehicleRepository):
         self.infraction_repo = infraction_repo
         self.user_repo = user_repo
+        self.vehicle_repo = vehicle_repo
 
     def get_infractions_by_plate_service(self, plate: str) -> Infractions:
+        try_plate = self.vehicle_repo.get_vehicle_by_plate(plate)
+        if not try_plate:
+            raise NotRegisteredVehicle("Placa informada não cadastrada no sistema")
         return self.infraction_repo.get_infractions_by_plate(plate)
 
     def get_queries_by_user_id_service(self, user_id: int) -> InfractionQueries:
